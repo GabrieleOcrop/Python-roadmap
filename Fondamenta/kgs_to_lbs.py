78c82d5e54bba4a07b2acb90c3add08404e96da4
Gabriele <gabrielepillitteri6@gmail.com>
2026-06-28 19:58:32 +0200

    Tracking for my gym progress

---
+++ Fondamenta/kgs_to_lbs.py
@@ -0,0 +1,7 @@
+#i use to convert my gym workouts weights or my weigh
+weigh_in_kgs = float(input("What's your kg you want to convert ? "))
+weigh_in_lbs = (weigh_in_kgs * 2.20462)
+
+print('Your input was ' + str(weigh_in_kgs) + 'kgs and that s ' + str(weigh_in_lbs) + 'lbs')
+
+
