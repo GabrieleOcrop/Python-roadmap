05059a780a8aef4ee541403439b050d0f4d5ad54
Gabriele <gabrielepillitteri6@gmail.com>
2026-06-28 20:41:09 +0200

    new stuff about arithmetical operators

---
+++ Fondamenta/arithmeticalexpression.py
@@ -0,0 +1,15 @@
+# + , -, * and / are arithmetical operators
+#for sum, substract, multiply and division
+#it will give the result
+print(10 / 3)
+#it will give the remainder
+print(10 % 3)
+# use a double star for elevating the number
+print(10 ** 2)
+
+#increment a number
+x = 10
+#x = x + 3 or
+#less characters and it work also with substract 
+x += 3
+print(x)
