7d55c37f9b161d593cb638f652df8b04aec5ecce
Gabriele <gabrielepillitteri6@gmail.com>
2026-07-03 08:41:10 +0200

    First exercise with for loop

---
+++ Fondamenta/Forexercise.py
@@ -0,0 +1,10 @@
+prices = [10, 25, 4, 6.5,2 , 9.9, 14, 2]
+for item, prices in enumerate(prices):
+    if prices >= 5:
+        print(f'The {item} item item that cost {prices} it too expensive')
+    else:
+        print(f'The {item} item that cost {prices} is cheaper')
+
+#() calls a function
+#[] join an element by an list, string or dictionary
+#{} creates a set or a dictionary
---
+++ Fondamenta/forloops.py
@@ -0,0 +1,4 @@
+#range method accepts 3 values
+#first is start, second is stop, third is range
+for item in range(10, 100, 5):
+    print(item)
