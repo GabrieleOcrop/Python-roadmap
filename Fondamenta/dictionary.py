22de53c9da4b411401ab39926a90d8519f415a08
GabrieleOcrop <gabrielepillitteri6@gmail.com>
2026-07-15 15:09:44 +0200

    Did a little cleaning code before i use commit well

--- Fondamenta/dictionary.py
+++ Fondamenta/dictionary.py
@@ -1,8 +1,23 @@
-Paziente = {
+'''Paziente = {
     "nome": "Gabriele Pillitteri",
     "età": 27,
     "E' stato visitato?": True
 }
 Paziente["Data di nascita"] = "Oct 10 1998"
-print(Paziente.get("Data di nascita", "Oct 10 1998"))
+#print(Paziente.get("Data di nascita", "Oct 10 1998"))'''
 
+Pazienti = {
+    "Paziente_1": "Gabriele",
+    "Paziente_2": "Giuseppe",
+    "Paziente_3": "Francesco",
+    "Paziente_4": "Marco"
+}
+
+Paziente = "Gabriele"
+#.values() per i valori interni, .keys() per le chiavi o comunque i nomi
+if Paziente in Pazienti.values():
+    status = 1
+else:
+    status = 0
+
+print(status)
--- Fondamenta/kcal_bmi_counter_v2.py
+++ Fondamenta/kcal_bmi_counter_v2.py
@@ -1,5 +1,10 @@
 import os
 
+#sets for user responses
+allowed_yes_responses = {"yes", "yep", "y", "sure"}
+allowed_no_responses = {"no", "nop", "n", "negative"}
+allowed_genre_responses = {"male", "female", "m", "f"}
+
 #functions
 
 #for cleaning console 
@@ -21,81 +26,81 @@ def get_name():
 
 def get_genre():
     while True:
-        genre = input("Are you male or female? :").strip()
+        genre = input("Are you male or female? :")
         if not genre:
             print("Genre field cannot be empty!")
-        elif ("male" not in genre.replace(" ", "").lower() and "female" not in genre.replace(" ", "").lower() ):
-            print("genre field doesn't accept other genres!")
-        elif any(char.isdigit() for char in name):
-            print("Genre field doesn't accept numbers")
-        elif (genre == "m" or genre == "M" or genre == "f" or genre == "F"):
-            return genre
+        elif genre.strip().lower() not in allowed_genre_responses:
+            print("genre field accept only male/female genres! Don't get offended :|")
         else:
             return genre
 
 def get_age():
     while True:
-        age = int(input("How old are you? :"))
+        age = input("How old are you? :")
         if not age:
             print("Age field cannot be empty!")
-        elif (age < 4):
+        elif not age.isdigit(): #is the string age full of numbers? yes; there are some characters into the string? error
+            print("Stop playing dude -.-")
+        elif (int(age) < 4):
             print("You are too child to use that! Go play around!")
-        elif (age > 110):
+        elif (int(age) > 110):
             print("Seems to be an error in age input!")
         else:
-            return age
+            return int(age)
 
 
 def get_height():
     while True:
-        height = float(input("How tall are you? Be sure that's in centimeters! :"))
+        height = input("How tall are you? Be sure that's in centimeters! :")
         if not height:
             print("Height field cannot be empty!")
+        elif not height.isdigit():
+            print("Stop playing dude -.-")
         else:
-            asking = input("Are you sure that you put the right value? :").lower().strip().replace(" ", "")
-            if "yes" in asking or "yeah" in asking or "yeh" in asking:
-                return height
-            else:
-                False
+            asking = input("Are you sure that you put the right value? :")
+            if asking.lower() in allowed_yes_responses:
+                return int(height)
 
 def get_weight():
     while True:
-        weight = float(input("How much do you weight? Be sure that's in kilograms! :"))
+        weight = input("How much do you weight? Be sure that's in kilograms! :")
         if not weight:
             print("Weight field cannot be empty!")
+        elif not weight.replace(".", "", 1).isdigit():
+            print("Stop playing dude -.-")
         else:
-            asking = input("Are you sure that you put the right value? :").lower().strip().replace(" ", "")
-            if "yes" in asking or "yeah" in asking or "yeh" in asking:
-                return weight
-            else:
-                False
+            asking = input("Are you sure that you put the right value? :")
+            if asking.lower() in allowed_yes_responses:
+                return float(weight)
 
 def get_workouts_per_week():
     while True:
-        workouts_per_week = int(input("How many workouts do you do per week? :"))
+        workouts_per_week = input("How many workouts do you do per week? :")
         if not workouts_per_week:
             print("Number of workouts per week field cannot be empty!")
-        elif (workouts_per_week < 0):
+        elif not workouts_per_week.isdigit():
+            print("stop playing dude -.-")
+        elif (int(workouts_per_week) < 0):
             print("Please stop! You can't do negative workouts!")
-        elif (workouts_per_week > 7):
+        elif (int(workouts_per_week) > 7):
             print("I dunno if you are too crazy to do this shit! Please take a team and don't use this crap! or revise :)")
         else:
-            return workouts_per_week
+            return int(workouts_per_week)
 
         
 
 
 def get_numero_daily_steps():
     while True:
-        daily_steps = int(input("How many steps do you usually take each day? :"))
+        daily_steps = input("How many steps do you usually take each day? :")
         if not daily_steps:
             print("passi giornalieri field cannot be empty!")
-        elif (daily_steps < 0):
-            print("Please stop! You can't take negative steps!")
-        elif (daily_steps > 20000):
+        elif not daily_steps.isdigit():
+            print("stop playing dude! -.-")
+        elif (int(daily_steps) > 20000):
             print("I dunno if you are too crazy to do this shit! Please take a team and don't use this crap! or revise :)")
         else:
-            return daily_steps
+            return int(daily_steps)
 
 
 def male_kcal(age,height,weight):
@@ -122,17 +127,17 @@ def bmi_calculation(height,weight):
 def daily_steps_score_calculator(daily_steps):
     if daily_steps >= 10000:
         return 5
-    elif daily_steps > 7500 and daily_steps < 10000:
+    elif daily_steps >= 7500:
         return 4
-    elif daily_steps > 5000 and daily_steps < 7500:
+    elif daily_steps >= 5000:
         return 3
-    elif daily_steps > 2500 and daily_steps < 5000:
+    elif daily_steps >= 2500:
         return 2
     elif daily_steps < 2500:
         return 1
     
 def workouts_per_week_calculator(workouts_per_week):
-    if 5 == workouts_per_week > 5:
+    if workouts_per_week >= 5:
         return 5
     elif workouts_per_week == 4:
         return 4
@@ -165,7 +170,7 @@ def TDEE_calculator(life_style_score, kcal_value):
 #main program
 clear_console()
 
-print("Kcal and bmi counter v3 made by nothing to do!")
+print("Kcal, bmi and TDEE counter v3 made by nothing to do!")
 while True:
     name = get_name()
     genre = get_genre()
@@ -179,30 +184,25 @@ while True:
     workouts_per_week_score = workouts_per_week_calculator(workouts_per_week)
     life_style_score = score_life_style_calculator(daily_steps_score, workouts_per_week_score)
     
-    input_data = f'Your name is {name} and you are a {genre}. You are {age} years old, you are tall {height}cm and your weight is {weight}kgs'
-    input_data_2 = f'Your daily steps is around {daily_steps} and you do {workouts_per_week} workouts every week' 
-    print(input_data + '/n ' + input_data_2)
-    choose = input("That's correct? :").strip().lower().replace(" ", "")
-    if "yes" in choose or "yeah" in choose or "yeh" in choose:
-        bmi_value = float(bmi_calculation(int(height), int(weight)))
-        if "female" in genre.lower().strip().replace(" ", ""):
-            kcal_value = female_kcal(int(age), int(height), int(weight))
+    input_data = f'Your name is {name} and you are a {genre}. You are {age} years old, you are tall {height}cm and your weight is {weight}kgs. \n Your daily steps is around {daily_steps} and you do {workouts_per_week} workouts every week'
+    print(input_data) 
+    choose = input("That's correct? :").lower()
+    if choose.lower() in allowed_yes_responses:
+        bmi_value = bmi_calculation(height,weight)
+        if genre.strip().lower() == "female" or genre.strip().lower() == "f":
+            kcal_value = female_kcal(age, height, weight)
             TDEE = TDEE_calculator(life_style_score, kcal_value)
-            message = f'Your BMI is {bmi_value:.2f} and your BMR {round(kcal_value)} per day!'
-            message_2 = f'Your movement score is {life_style_score} so you have to stay around {TDEE} kcals per day!'
+            message = f'Your BMI is {bmi_value:.2f} and your BMR {round(kcal_value)} per day! \n Your life_style score is {life_style_score} so you have to stay around {round(TDEE)} kcals per day!'
             print(message)
-            print(message_2)
             break
-        elif "male" in genre.lower().strip().replace(" ", ""):
-            kcal_value = male_kcal(int(age), int(height), int(weight))
+        elif genre.strip().lower() == "male" or genre.strip().lower() == "m":
+            kcal_value = male_kcal(age, height, weight)
             TDEE = TDEE_calculator(life_style_score, kcal_value)
-            message = f'Your BMI is {bmi_value:.2f} and your BMR {round(kcal_value)} per day!'
-            message_2 = f'Your movement score is {life_style_score} so you have to stay around {TDEE} kcals per day!'
+            message = f'Your BMI is {bmi_value:.2f} and your BMR {round(kcal_value)} per day! \n Your movement score is {life_style_score} so you have to stay around {round(TDEE)} kcals per day!'
             print(message)
-            print(message_2)
             break
-    elif "no" in choose or "nah" in choose or "nope" in choose:
+    elif choose.lower() in allowed_no_responses:
         continue
     else:
-        continue
+        break
 
